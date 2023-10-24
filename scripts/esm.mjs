60206e07bea8e26220228eb97ffea7c8c973a2b4
João Gabriel Tonaco <blu3zin@gmail.com>
2023-10-24 19:04:43 -0300

    no more src and build dirs

---
+++ scripts/esm.mjs
@@ -0,0 +1,25 @@
+import { exec } from "node:child_process";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { promisify } from "node:util";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const rootDir = join(__dirname, "..");
+const execAsync = promisify(exec);
+
+/**
+ * @param {string} path
+ * @param {string} version
+ */
+const fileToESMWrapperCall = (path, version) =>
+  execAsync(`npx gen-esm-wrapper "${join(rootDir, path, `${version}.js`)}" "${join(rootDir, path, `${version}.mjs`)}"`);
+
+await Promise.allSettled(
+  ["v2"].map((version) => [
+    fileToESMWrapperCall(`payloads/${version}`, "index"),
+    fileToESMWrapperCall(`rest/${version}`, "index"),
+    fileToESMWrapperCall("", version),
+    fileToESMWrapperCall("", "utils"),
+  ]),
+);
