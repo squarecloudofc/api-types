59da44325a922ebe932498c69a9a2002dc8d25d8
João Gabriel Tonaco <blu3zin@gmail.com>
2024-02-04 09:15:17 -0300

    update plans enum to new names

---
+++ .changeset/poor-toys-allow.md
@@ -0,0 +1,5 @@
+---
+"@squarecloud/api-types": minor
+---
+
+update `UserPlanName` enum to new plans
--- payloads/v2/user.ts
+++ payloads/v2/user.ts
@@ -6,13 +6,10 @@ import { ApplicationLanguage } from "./application";
  * @see https://docs.squarecloud.app/api-reference/endpoint/user/info
  */
 export enum UserPlanName {
-  Free = "free",
-  Basic = "basic",
-  Medium = "medium",
-  Advanced = "advanced",
-  Senior = "senior",
-  Deluxe = "deluxe",
-  Orion = "orion",
+  Hobby = "hobby",
+  Standard = "standard",
+  Pro = "pro",
+  Business = "business",
   Enterprise32 = "enterprise-32",
   Enterprise48 = "enterprise-48",
   Enterprise64 = "enterprise-64",
@@ -22,6 +19,11 @@ export enum UserPlanName {
   Enterprise192 = "enterprise-192",
   Enterprise224 = "enterprise-224",
   Enterprise256 = "enterprise-256",
+  Enterprise288 = "enterprise-288",
+  Enterprise320 = "enterprise-320",
+  Enterprise384 = "enterprise-384",
+  Enterprise448 = "enterprise-448",
+  Enterprise512 = "enterprise-512",
 }
 
 /**
