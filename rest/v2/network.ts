58d009dcc611f57ab39c3451ad6be2d4239d1235
João Gabriel Tonaco User <joao.tonaco@proton.me>
2024-07-22 20:12:44 -0300

    feat: typings for app network custom

--- rest/v2/network.ts
+++ rest/v2/network.ts
@@ -3,4 +3,11 @@ import type { APIPayloadStatusOnly } from "../../common/v2";
 /**
  * @see https://docs.squarecloud.app/api-reference/endpoint/apps/network/custom
  */
+export interface RESTPostAPINetworkCustomDomainJSONBody {
+	custom: string;
+}
+
+/**
+ * @see https://docs.squarecloud.app/api-reference/endpoint/apps/network/custom
+ */
 export type RESTPostAPINetworkCustomDomainResultPayload = APIPayloadStatusOnly;
